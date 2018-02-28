1ff99edbfc5939d7b00f65028f96a5d448053c6e
stella <3291570994@qq.com>
2018-02-28 17:29:14 +0800

    修改index

---
+++ src/components/Pos.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="pos">
+        hello pos demo1
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'pos.vue'
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Pos from '@/components/Pos'
+
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    routes: [
+        {
+            path: '/',
+            name: 'Pos',
+            component: Pos
+        }
 
-  ]
+    ]
 })
